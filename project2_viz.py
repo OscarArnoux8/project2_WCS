211b5655f4403b8ba9a4ae9ee5314255e40395fd
OscarArnoux8 <97617063+OscarArnoux8@users.noreply.github.com>
2022-05-05 14:54:57 +0200

    Update project2_viz.py

--- project2_viz.py
+++ project2_viz.py
@@ -12,7 +12,7 @@ imdb = pd.read_csv('./imdb.csv')
 st.title('Visualisations et indicateurs')
 st.write('Le résultat de nos analyses sur l\'industrie du film')
 
-fig,(ax1,ax2) = plt.subplots(1,2,sharex=True,sharey=True)
+fig,(ax1,ax2) = plt.subplots(1,2)
 
 ax1 = imdb.groupby(imdb.startYear)['numVotes'].mean().plot()
 plt.title('Evolution de la Moyenne des votes dans le temps')
@@ -24,4 +24,7 @@ plt.title('Notes en fonction de la durée des films')
 plt.xlabel('Durée (minutes)')
 plt.ylabel('Note moyenne')
 
+for ax in axs.flat:
+    ax.label_outer()
+    
 st.pyplot(fig)
