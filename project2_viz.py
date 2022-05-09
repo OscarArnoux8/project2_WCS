46c98c9f8eab78009a326be821d219d523f9bb87
Oscar Arnoux <97617063+OscarArnoux8@users.noreply.github.com>
2022-05-09 07:47:33 +0200

    Update project2_viz.py

--- project2_viz.py
+++ project2_viz.py
@@ -57,7 +57,7 @@ with row_2_2, _lock:
   plt.ylabel('Note moyenne')
   st.pyplot(fig4)
 
-  with row_2_3, _lock:
+with row_2_3, _lock:
   fig5,ax5 = plt.subplots() # Fourth graph, average rating overtime
   ax5 = sns.lineplot(data=imdb,x='startYear',y='averageRating')
   plt.title('Evolution de la Moyenne des notes dans le temps')
@@ -65,25 +65,35 @@ with row_2_2, _lock:
   plt.ylabel('Note moyenne')
   st.pyplot(fig5)
 
-plus_notes = imdb.numVotes.sort_values(ascending=False).head(10) # Our top 10 movies by number of votes, as a table
-titres = np.array([imdb.title.iloc[plus_notes.index[i]] for i in range(10)])
-votes = np.array([imdb.numVotes.iloc[plus_notes.index[i]] for i in range(10)])
-plus_notes = pd.DataFrame([{titres[x]:str(votes[x]) for x in range(10)}]).T 
-st.table(plus_notes.rename(columns={0:'Nombre de votes'}))
+row3_space1, row_3_1, row3_space2, row_3_2 = st.columns((.1, 1, .1, 1))
 
-mieux_notes = imdb.averageRating.sort_values(ascending=False).head(10) # Our top 10 movies by average rating, as a table
-titres_notes = np.array([imdb.title.iloc[mieux_notes.index[i]] for i in range(10)])
-notes = np.array([imdb.averageRating.iloc[mieux_notes.index[i]] for i in range(10)])
-mieux_notes = pd.DataFrame([{titres_notes[x]:str(notes[x])[0:3] for x in range(10)}]).T 
-st.table(mieux_notes.rename(columns={0:'Note moyenne'}))
+with row_3_1, _lock:
+  plus_notes = imdb.numVotes.sort_values(ascending=False).head(10) # Our top 10 movies by number of votes, as a table
+  titres = np.array([imdb.title.iloc[plus_notes.index[i]] for i in range(10)])
+  votes = np.array([imdb.numVotes.iloc[plus_notes.index[i]] for i in range(10)])
+  plus_notes = pd.DataFrame([{titres[x]:str(votes[x]) for x in range(10)}]).T 
+  st.table(plus_notes.rename(columns={0:'Nombre de votes'}))
 
-fig5,ax5 = plt.subplots() # Fifth graph, amount of movies for each genre
-ax5= people.genres.str.get_dummies(',').sum().sort_values(ascending=True).tail(15).plot(kind='barh')
-plt.ylabel('Genre')
-plt.xlabel('Nombre de films')
-plt.title('Films par genre')
-st.pyplot(fig5)
+with row_3_2, _lock:
+  mieux_notes = imdb.averageRating.sort_values(ascending=False).head(10) # Our top 10 movies by average rating, as a table
+  titres_notes = np.array([imdb.title.iloc[mieux_notes.index[i]] for i in range(10)])
+  notes = np.array([imdb.averageRating.iloc[mieux_notes.index[i]] for i in range(10)])
+  mieux_notes = pd.DataFrame([{titres_notes[x]:str(notes[x])[0:3] for x in range(10)}]).T 
+  st.table(mieux_notes.rename(columns={0:'Note moyenne'}))
 
+row4_space1, row_4_1, row4_space2, row_4_2 = st.columns((.1, 1, .1, 1))
+
+with row_4_1, _lock:
+  fig5,ax5 = plt.subplots() # Fifth graph, amount of movies for each genre
+  ax5= people.genres.str.get_dummies(',').sum().sort_values(ascending=True).tail(15).plot(kind='barh')
+  plt.ylabel('Genre')
+  plt.xlabel('Nombre de films')
+  plt.title('Films par genre')
+  st.pyplot(fig5)
+
+# Add graph with multiple genres
+
+# This is the fifth row, maybe add a graph with evolution of gender repartition ?
 fig6,ax6 = plt.subplots() # Sixth graph, repartition by sex : percentage of actresses and actors
 ax6 = round(people.category.value_counts(normalize=True)*100,1).plot(kind='bar')
 plt.ylabel('Pourcentage')
@@ -92,15 +102,12 @@ ax6.bar_label(ax6.containers[0], label_type='edge')
 plt.title('Répartition par sexe')
 st.pyplot(fig6)
 
-fig2,ax2 = plt.subplots() # Seventh graph, amount of movies by decade
-ax7 = imdb.groupby((imdb['startYear']//10)*10)['tconst'].count().plot(kind='barh')
-plt.ylabel('Décennie')
-plt.xlabel('Nombre de films')
-plt.title('Sorties par décennie')
-st.pyplot(fig7)
+row6_space1, row_6_1, row6_space2, row_6_2 = st.columns((.1, 1, .1, 1))
 
-top_actors = pd.DataFrame(people.query('category=="actor"').primaryName.value_counts().head()) # Table of the 5 most prolific actors
-st.dataframe(top_actors.rename(columns={'primaryName':'Acteurs'}))
-
-top_actresses = pd.DataFrame(people.query('category=="actress"').primaryName.value_counts().head()) # Table of the 5 most prolific actresses
-st.dataframe(top_actresses.rename(columns={'primaryName':'Actrices'}))
+with row_6_1, _lock:
+  top_actors = pd.DataFrame(people.query('category=="actor"').primaryName.value_counts().head()) # Table of the 5 most prolific actors
+  st.dataframe(top_actors.rename(columns={'primaryName':'Acteurs'}))
+  
+with row_6_2, _lock:
+  top_actresses = pd.DataFrame(people.query('category=="actress"').primaryName.value_counts().head()) # Table of the 5 most prolific actresses
+  st.dataframe(top_actresses.rename(columns={'primaryName':'Actrices'}))
