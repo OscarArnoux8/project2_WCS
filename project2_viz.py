2fecb7af40bf7cfd9d33b0ed5319a032b60b6f11
Oscar Arnoux <97617063+OscarArnoux8@users.noreply.github.com>
2022-05-09 23:05:20 +0200

    Update project2_viz.py

--- project2_viz.py
+++ project2_viz.py
@@ -40,11 +40,9 @@ with coltab:
                        ['ratings','1 230 633']]),
                      columns = ['Nom','Lignes']))
 with colpie:
-  width = st.sidebar.slider("plot width", 1, 25, 3)
-  height = st.sidebar.slider("plot height", 1, 25, 1)
-  fig, ax = plt.subplots(figsize=(width, height))
+  fig, ax = plt.subplots(figsize=(2,2))
   ax = plt.pie(x=[605284-22560,22560],labels=['96,3%','3,7%'],explode=[0,0.1])
-  plt.legend(['Total','Notre sélection'])
+  plt.title('Part de la base extraite vs total')
   st.pyplot(fig)
   
 st.write(''' En réalité, la base de donnée contient 605 284 films.
