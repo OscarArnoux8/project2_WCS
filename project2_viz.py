c9675966e56a6a1234dee877c8101bd56b944ea5
Oscar Arnoux <97617063+OscarArnoux8@users.noreply.github.com>
2022-05-09 06:56:56 +0200

    Update project2_viz.py

--- project2_viz.py
+++ project2_viz.py
@@ -13,6 +13,8 @@ people = pd.read_csv('./people.csv') # Our table with actors/actresses
 st.title('Visualisations et indicateurs') # The title section
 st.write('Le résultat de nos analyses sur l\'industrie du film')
 
+col1, col2 = st.columns(2)
+
 fig1,ax1 = plt.subplots() # First graph : lineplot, movies by year (maybe try with sns/px ?)
 ax1 = imdb.groupby(imdb.startYear)['numVotes'].mean().plot()
 plt.title('Evolution du nombre de votes par film dans le temps')
