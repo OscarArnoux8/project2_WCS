c903f7bd65f28550b34fed289f11a09c35295ecc
Oscar Arnoux <97617063+OscarArnoux8@users.noreply.github.com>
2022-05-09 22:56:37 +0200

    Update project2_viz.py

--- project2_viz.py
+++ project2_viz.py
@@ -41,8 +41,8 @@ with coltab:
                      columns = ['Nom','Lignes']))
 with colpie:
   fig,ax = plt.subplots()
-  ax = plt.pie(x=[605284-22560,22560],labels=['Total','Notre sélection'],explode=[0,0.1])
-  plt.legend(['e','s'])
+  ax = plt.pie(x=[605284-22560,22560],labels=['96,3%','3,7%'],explode=[0,0.1])
+  plt.legend(['Total','Notre sélection'])
   st.pyplot(fig)
   
 st.write(''' En réalité, la base de donnée contient 605 284 films.
