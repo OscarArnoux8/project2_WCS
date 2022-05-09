ed6dc9aa4e8dda8fceb56281a5e36259d5fc471a
Oscar Arnoux <97617063+OscarArnoux8@users.noreply.github.com>
2022-05-09 07:29:20 +0200

    Update project2_viz.py

--- project2_viz.py
+++ project2_viz.py
@@ -23,7 +23,7 @@ st.write('Le résultat de nos analyses sur l\'industrie du film')
 
 row1_space1, row_1, row1_space2, row2 = st.columns((.1, 1, .1, 1))
 
-with row1, _lock:
+with row_1, _lock:
   fig1,ax1 = plt.subplots() # First graph : lineplot, movies by year (maybe try with sns/px ?)
   ax1 = imdb.groupby(imdb.startYear)['numVotes'].mean().plot()
   plt.title('Evolution du nombre de votes par film dans le temps')
