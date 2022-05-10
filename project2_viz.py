44d4c09d3f025ae09a3f742c3cd28a14c4ac371c
Oscar Arnoux <97617063+OscarArnoux8@users.noreply.github.com>
2022-05-10 10:05:12 +0200

    Update project2_viz.py

--- project2_viz.py
+++ project2_viz.py
@@ -19,8 +19,8 @@ _lock = RendererAgg.lock
 st.set_page_config(layout="wide")
 
 st.write(f'''
-    <a target="_self" href="https://share.streamlit.io/oscararnoux8/projet2_wcs/main/project2_viz.py" style="text-decoration: none">
-        <button>
+    <a target="_self" href="https://share.streamlit.io/pilouliz/movie_reco/main/app.py" style="text-decoration: none">
+        <button kind="primary" class="css-1q8dd3e edgvbvh9">
             Nos recommandations
         </button>
     </a>
