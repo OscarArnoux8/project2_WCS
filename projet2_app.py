896de7b3ea8c61e96a3acbc1b9411247bebe5630
Oscar Arnoux <97617063+OscarArnoux8@users.noreply.github.com>
2022-05-11 19:16:09 +0200

    Update projet2_app.py

--- projet2_app.py
+++ projet2_app.py
@@ -98,7 +98,8 @@ with st.sidebar:
 
 
 def get_OMDB(movieID):
-  OMDB = requests.get('http://www.omdbapi.com/?i='+ movieID + '&apikey=' + st.secrets["key"]).json()
+  #OMDB = requests.get('http://www.omdbapi.com/?i='+ movieID + '&apikey=' + st.secrets["key"]).json()
+  OMDB = requests.get('http://www.omdbapi.com/?i=' + movieID + 'apikey=14283348').json()
   return OMDB
 
 cols = st.columns(len(setting_name))
